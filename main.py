444da55d6ae97cf1fabd88b81aa303aca1c1bed5
NolanThrondset2022 <816059@mystma.org>
2021-09-28 14:57:40 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 #WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x > y:
+        return True
+    else:
+        return False
+def lessThan(x,y):
+    if x < y:
+        return True
+    else:
+        return False
+def equalTo(x,y):
+    if x == y:
+        return True
+    else:
+        return False
+def greaterOrEqual(x,y):
+    if x >= y:
+        return True
+    else:
+        return False 
+def lessOrEqual(x,y):
+    if x <= y:
+        return True
+    else:
+        return False
+
+print(greaterThan(10,6))
+print(lessThan(10,6))
+print(equalTo(10,10))
+print(greaterOrEqual(5,10))
+print(lessOrEqual(7,7))
